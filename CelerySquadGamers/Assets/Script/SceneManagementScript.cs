564aebcdebc3125b3566e02b282cd00a75c84413
Lunick <karimelzanaty98@gmail.com>
2018-10-27 20:41:42 -0400

    Added the menu scenes

---
+++ CelerySquadGamers/Assets/Script/SceneManagementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementScript : MonoBehaviour {
+
+	public string gameSceneName;
+	public string rulesSceneName;
+	public string titleSceneName;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(gameSceneName);
+	}
+
+	public void GoToRules()
+	{
+		SceneManager.LoadScene(rulesSceneName);
+	}
+
+	public void GoToTitle()
+	{
+		SceneManager.LoadScene(titleSceneName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
